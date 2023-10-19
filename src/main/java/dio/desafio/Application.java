7ce10b33674c2b0bd38b53ca99dd1baa0a6b2a32
Siigmaf <faby.tbe@gmail.com>
2023-10-19 16:16:29 -0300

    feat:add model package

--- src/main/java/dio/desafio/Application.java
+++ src/main/java/dio/desafio/Application.java
@@ -2,7 +2,19 @@ package dio.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Este projeto Spring Boot faz parte do Desafio sobre padrões de projeto Spring Java do Bootcamp Santander & Digital Innovation One - JAVA.
+ * Foi gerado usando o Spring Initializr e inclui os seguintes módulos:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ * 
+ * Autor: Ignacio-fabianamaria
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
